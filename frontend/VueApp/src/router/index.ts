6ca8f0cc28906d1365a550d37c9d602c417e288f
Владислав Мышковский <vladislavmyshkovskiiy@gmail.com>
2025-10-23 20:30:18 +0300

    База фронта

--- frontend/VueApp/src/router/index.ts
+++ frontend/VueApp/src/router/index.ts
@@ -1,8 +1,16 @@
+import LoanApplicationsDirectory from '@/components/directories/LoanApplicationsDirectory.vue';
+import LoanApplicationCreateForm from '@/components/forms/create/LoanApplicationCreateForm.vue';
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', redirect: '/loans' },
+  { path: '/loans', component: LoanApplicationsDirectory },
+  { path: '/loans/create', component: LoanApplicationCreateForm }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
 
 export default router
